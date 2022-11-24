7cf73768564f6848eb0f4ea998d45fb7afcf1b29
Afv1 <a.lark@mail.ru>
2022-11-24 11:05:57 +0300

    add AuthUser func to ORM interface

--- pkg/repository/orm.go
+++ pkg/repository/orm.go
@@ -6,11 +6,9 @@ import (
     "github.com/afv1/sims-tree/pkg/dto"
 )
 
-type myORM struct {
-}
-
 type ORM interface {
     CreateUser(ctx context.Context, data dto.AuthUser) (*ent.User, error)
+    AuthUser(ctx context.Context, data dto.AuthUser) (*ent.User, error)
     QueryUser(ctx context.Context, data dto.AuthUser) (*ent.User, error)
     EditUser(ctx context.Context, data dto.AuthUser) (*ent.User, error)
     DeleteUser(ctx context.Context, data dto.AuthUser) error
