f56dce6ae8b6aa11d30efab0d646651a0f6f8794
Afv1 <a.lark@mail.ru>
2022-11-24 11:13:27 +0300

    add hashing function description

--- pkg/service/secret.go
+++ pkg/service/secret.go
@@ -11,6 +11,7 @@ import (
 
 const secret = "R6IFofjN4LzZ6YEt6IllVQrQ"
 
+// generatePasswordHash hashes password with HMAC-SHA256 algo
 func generatePasswordHash(pass string) string {
     h := hmac.New(sha256.New, []byte(secret))
     h.Write([]byte(pass))
@@ -18,6 +19,7 @@ func generatePasswordHash(pass string) string {
     return hex.EncodeToString(h.Sum(nil))
 }
 
+// generateCookie creates cookie with HMAC-SHA256 algo based on Unix timestamp
 func generateCookie(input string) string {
     h := hmac.New(sha256.New, []byte(cfg.App.AuthSessKey))
     h.Write([]byte(input + fmt.Sprintf("%d", time.Now().Unix())))
